d8fa81f5303096579095dcb9adaa115687b7bd4a
yurh <airson_yu@163.com>
2019-06-18 10:38:39 +0800

    init

---
+++ src/main/java/com/lhfeiyu/tech/eureka/server/TechEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.lhfeiyu.tech.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechEurekaServerApplication.class, args);
+    }
+
+}

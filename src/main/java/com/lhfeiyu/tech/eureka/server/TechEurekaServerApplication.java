4bb460959f63d5373f0e4b749c93db8606d0f32c
yurh <airson_yu@163.com>
2019-06-20 23:28:28 +0800

    t

--- src/main/java/com/lhfeiyu/tech/eureka/server/TechEurekaServerApplication.java
+++ src/main/java/com/lhfeiyu/tech/eureka/server/TechEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.lhfeiyu.tech.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class TechEurekaServerApplication {
 
